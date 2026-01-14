e3802e74fec63e555851db410cd2409f88e654e2
brij05 <108320592+brij05@users.noreply.github.com>
2026-01-14 11:57:51 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gigflow-backend-aylc.onrender.com",
+  baseURL: "https://gigflow-backend-aylc.onrender.com/api/auth/register",
   withCredentials: true, 
 });
 
